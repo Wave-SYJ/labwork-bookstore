8634b2fae438ae9a433fe71a867c11599e11aa22
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-08-09 22:43:22 +0800

    引入ant design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../assets/style/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default MyApp;
+export default App;
