0bfdd27bcbe35baa841d570216200d42862fe9c4
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-08-10 22:04:40 +0800

    first commit

---
+++ src/main/java/cn/edu/seu/bookstore/LabworkBookstoreServerApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.seu.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabworkBookstoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabworkBookstoreServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/seu/bookstore/LabworkBookstoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.seu.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabworkBookstoreServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
